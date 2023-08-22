c108400136285b9a0812bf40a2c2569c81c248ae
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-08-22 18:35:26 +0300

    complete assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -6,9 +6,9 @@ function BlogPost() {
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
+      <Comment commentText="I agree with this statement.-Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths.-Sonmi-451" />
     </div>
   );
 }
